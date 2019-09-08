1ffba22830c9c29957c44ab56fc4f470f104690f
lisamcc <lisamccormack85@gmail.com>
2019-09-08 10:56:34 +0100

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,31 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size;
+  }
+  return chunked;
+}
+// the slice method returns the selected elements in an array as a new array object.
+// the slice method selects the elements starting at the given start argument and ends at,
+// but does not include, the given end argument.
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   const chunked = [];
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//
+//     if(!last || last.length === size){
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
